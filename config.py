9fda8502c5997762f48891d6f49c872e52f45651
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-11 10:50:49 +0200

    Added Config files and .env.example

---
+++ config.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
+    class Config:
+        env_file: str = ".env"
+
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_")
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: str = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    model_config = SettingsConfigDict(env_prefix="TEST_")
+
