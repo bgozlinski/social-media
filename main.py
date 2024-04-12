12093c5dac70237f9ba8a2c11eab627b335879c2
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-12 12:25:48 +0200

    Added lifespan connection to db in main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
 from routers.post import router as post_router
+from contextlib import asynccontextmanager
+from database import database
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Asynchronous context manager for managing the lifespan of the application.
+
+    This context manager ensures that the database connection is established
+    when the application starts and cleanly disconnected when the application
+    stops. This is crucial for resources that need to be released properly
+    to avoid resource leaking or other side effects.
+
+    Args:
+    app (FastAPI): An instance of the FastAPI application to which this lifespan
+                   is tied.
+
+    Yields:
+    None: This context manager does not yield any value but ensures database
+          connection management.
+    """
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router, prefix="")
