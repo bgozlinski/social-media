677a6c93c766742ef985e7298a8e09f05d76b63f
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-19 15:05:58 +0200

    Added create_access_token and authenticate_user

--- src/security.py
+++ src/security.py
@@ -1,12 +1,47 @@
 import logging
+import datetime
+from typing import Optional
+
+from fastapi import HTTPException, status
+
+from jose import jwt
+
 from src.database import database, user_table
 from passlib.context import CryptContext
+
+from src.config import config
 
 # Configure logger for the current module
 logger = logging.getLogger(__name__)
 
 # Create a password context for hashing passwords using bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+
+authenticate_user_credentials_exceptions = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                                         detail="Could not validate credentials"
+                                                         )
+
+
+def access_token_expire_minutes() -> int:
+    return 30
+
+
+def create_access_token(email: str) -> str:
+    logger.debug(f"Creating access token for {email}")
+
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=access_token_expire_minutes())
+
+    jwt_data = {
+        "sub": email,
+        "exp": expire,
+    }
+
+    encoded_jwt = jwt.encode(claims=jwt_data,
+                             key=config.SECRET_KEY,
+                             algorithm=config.ALGORITHM
+                             )
+    return encoded_jwt
 
 
 def get_password_hash(password: str) -> str:
@@ -36,7 +71,7 @@ def verify_password(plain_pwd: str, hashed_pwd: str) -> bool:
     return pwd_context.verify(plain_pwd, hashed_pwd)
 
 
-async def get_user(email: str) -> dict | None:
+async def get_user(email: str) -> Optional[dict]:
     """
     Asynchronously retrieve a user's record from the database by their email address.
 
@@ -58,3 +93,15 @@ async def get_user(email: str) -> dict | None:
 
     if result:
         return result
+
+
+async def authenticate_user(email: str, password: str) -> dict:
+    logger.debug(f"Authenticating user {email}")
+    user = await get_user(email)
+
+    if not user:
+        raise authenticate_user_credentials_exceptions
+    if not verify_password(password, user.password):
+        raise authenticate_user_credentials_exceptions
+
+    return user
