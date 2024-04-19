b2aa8ee3c494e6ac49dcf304603c2dd834ef1aec
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-19 14:10:21 +0200

    New test to veryfi if password hashing works properly.

--- src/tests/test_security.py
+++ src/tests/test_security.py
@@ -2,6 +2,11 @@ import pytest
 from src import security
 
 
+def test_password_hashing():
+    password = "password"
+    assert security.verify_password(password, security.get_password_hash(password))
+
+
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
     user = await security.get_user(registered_user["email"])
