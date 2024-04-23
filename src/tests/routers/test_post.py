3ed11f0188efefd042f5b14fcf9102a9756a77ab
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-23 23:18:29 +0200

    New tests

--- src/tests/routers/test_post.py
+++ src/tests/routers/test_post.py
@@ -1,38 +1,55 @@
 import pytest
 from httpx import AsyncClient
+from src import security
 
 
-async def create_post(body: str, async_client: AsyncClient) -> dict:
-    response = await async_client.post("/post", json={"body": body})
+async def create_post(body: str,
+                      async_client: AsyncClient,
+                      logged_in_token: str
+                      ) -> dict:
+    response = await async_client.post("/post",
+                                       json={"body": body},
+                                       headers={"Authorization": f"Bearer {logged_in_token}"}
+                                       )
     return response.json()
 
 
-async def create_comment(body: str, post_id: int, async_client: AsyncClient) -> dict:
+async def create_comment(body: str,
+                         post_id: int,
+                         async_client: AsyncClient,
+                         logged_in_token: str
+                         ) -> dict:
     response = await async_client.post(
         "/comment",
-        json={"body": body, "post_id": post_id}
+        json={"body": body, "post_id": post_id},
+        headers={"Authorization": f"Bearer {logged_in_token}"}
     )
 
     return response.json()
 
 
 @pytest.fixture()
-async def created_post(async_client: AsyncClient):
-    return await create_post(body="Test Post", async_client=async_client)
+async def created_post(async_client: AsyncClient, logged_in_token: str):
+    return await create_post(body="Test Post", async_client=async_client, logged_in_token=logged_in_token)
 
 
 @pytest.fixture()
-async def created_comment(async_client: AsyncClient, created_post: dict):
-    return await create_comment(body="Test Comment", post_id=created_post["id"], async_client=async_client)
+async def created_comment(async_client: AsyncClient, created_post: dict, logged_in_token: str):
+    return await create_comment(body="Test Comment",
+                                post_id=created_post["id"],
+                                async_client=async_client,
+                                logged_in_token=logged_in_token
+                                )
 
 
 @pytest.mark.anyio
-async def test_create_post(async_client: AsyncClient):
+async def test_create_post(async_client: AsyncClient, logged_in_token: str):
     body = "Test Post"
 
     response = await async_client.post(
         "/post",
-        json={"body": body}
+        json={"body": body},
+        headers={"Authorization": f"Bearer {logged_in_token}"}
     )
 
     assert response.status_code == 201
@@ -40,10 +57,25 @@ async def test_create_post(async_client: AsyncClient):
 
 
 @pytest.mark.anyio
-async def test_create_post_missing_data(async_client: AsyncClient):
+async def test_create_post_when_token_expired(async_client: AsyncClient, registered_user: dict, mocker):
+    mocker.patch("src.security.access_token_expire_minutes", return_value=-1)
+    token = security.create_access_token(registered_user["email"])
     response = await async_client.post(
         "/post",
-        json={}
+        json={"body": "Test Post"},
+        headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == 401
+    assert "Token expired" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_create_post_missing_data(async_client: AsyncClient, logged_in_token: str):
+    response = await async_client.post(
+        "/post",
+        json={},
+        headers={"Authorization": f"Bearer {logged_in_token}"}
     )
 
     assert response.status_code == 422
@@ -58,14 +90,15 @@ async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
 
 
 @pytest.mark.anyio
-async def test_create_comment(async_client: AsyncClient, created_post: dict):
+async def test_create_comment(async_client: AsyncClient, created_post: dict, logged_in_token: str):
     body = "Test Comment"
 
     response = await async_client.post(
         "/comment",
         json={"body": body,
               "post_id": created_post["id"]
-              }
+              },
+        headers={"Authorization": f"Bearer {logged_in_token}"}
     )
 
     assert response.status_code == 201
