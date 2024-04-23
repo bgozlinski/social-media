69bcbc09252e8970ea56a1e5ed691e4e4d311b3a
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-23 23:37:54 +0200

    Added user_id

--- src/models/post.py
+++ src/models/post.py
@@ -9,6 +9,7 @@ class UserPostIn(BaseModel):
 class UserPost(UserPostIn):
     model_config = ConfigDict(from_attributes=True)
     id: int
+    user_id: int
 
 
 class CommentIn(BaseModel):
@@ -19,6 +20,7 @@ class CommentIn(BaseModel):
 class Comment(CommentIn):
     model_config = ConfigDict(from_attributes=True)
     id: int
+    user_id: int
 
 
 class UserPostWithComments(BaseModel):
