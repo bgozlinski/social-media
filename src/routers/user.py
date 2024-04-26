dddd0b8d15499423e77e36d9fee2bacb8ee42ce8
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-04-26 15:24:04 +0200

    Added confirm_email endpoint

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,10 +1,15 @@
 import logging
-
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Request
 from src.models.user import UserIn
-from src.security import get_user, get_password_hash, authenticate_user, create_access_token
 from src.database import database, user_table
-
+from src.security import (
+    get_user,
+    get_password_hash,
+    authenticate_user,
+    create_access_token,
+    get_subject_for_token_type,
+    create_confirmation_token
+)
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +17,10 @@ router = APIRouter()
 
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
-async def register(user: UserIn):
+async def register(
+        user: UserIn,
+        request: Request
+):
     if await get_user(user.email):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -30,7 +38,11 @@ async def register(user: UserIn):
 
     await database.execute(query)
     return {
-        "detail": "User registered successfully"
+        "detail": "User created successfully. Please confirm your email.",
+        "confirmation_url": request.url_for(
+            "confirm_email",
+            token=create_confirmation_token(user.email)
+        ),
     }
 
 
@@ -42,4 +54,19 @@ async def login(user: UserIn):
     return {
         "access_token": access_token,
         "token_type": "bearer"
+    }
+
+
+@router.get('/confirm/{token}')
+async def confirm_email(token: str):
+    email = get_subject_for_token_type(token=token, type="confirmation")
+    query = (
+        user_table.update().where(user_table.c.email == email).values(confirmed=True)
+    )
+
+    logger.debug(query)
+
+    await database.execute(query)
+    return {
+        "detail": "User confirmed"
     }
